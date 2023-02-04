375eca254beefeeb9b7e04251b607e2a8129f445
William Ray <111171001+Will513r@users.noreply.github.com>
2023-02-04 04:10:39 -0500

    added resume download

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var trickBtn = document.querySelector(".trick");
 
 trickBtn.addEventListener("mouseover", function() {
-  this.textContent = "LOL";
+  this.textContent = "LOL Just Kidding";
 })
 trickBtn.addEventListener("mouseout", function() {
   this.textContent = "Click Me";
