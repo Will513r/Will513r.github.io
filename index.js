13467e02b1197c0edb3b707d9a82bf38806abb1c
William <will513r@gmail.com>
2024-03-15 14:39:35 -0400

    Working on the layout added pictures

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+
+import "nes.css/css/nes.min.css";
+
 var trickBtn = document.querySelector(".trick");
 
-trickBtn.addEventListener("mouseover", function() {
+trickBtn.addEventListener("mouseover", function () {
   this.textContent = "LOL Just Kidding";
-})
-trickBtn.addEventListener("mouseout", function() {
+});
+trickBtn.addEventListener("mouseout", function () {
   this.textContent = "Click Me";
-})
-
+});
 
 var i = 0;
 var txt = document.getElementById("ab-me-p");
@@ -20,9 +22,3 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
-
-
-
-
-
-
